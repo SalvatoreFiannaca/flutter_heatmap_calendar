647fbdb2a646d85832348c683a9eeb1b180975c3
DESKTOP-4H0TE01 <salvatore.fiannaca2@gmail.com>
2023-01-21 16:00:03 +0100

    typo

--- lib/src/widget/heatmap_page.dart
+++ lib/src/widget/heatmap_page.dart
@@ -154,6 +154,7 @@ class HeatMapPage extends StatelessWidget {
             HeatMapWeekText(
               margin: margin,
               fontSize: fontSize,
+              locale: locale,
               size: size,
               fontColor: textColor,
             ),
